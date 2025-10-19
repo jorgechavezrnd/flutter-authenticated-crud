118c269aaa80d40ecca132bc90085d91e4a697b1
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-19 09:47:26 -0400

    Add implementation for login function in Auth Datasource with mapper and errors

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_app/config/config.dart';
 import 'package:teslo_app/features/auth/domain/domain.dart';
+import 'package:teslo_app/features/auth/infrastructure/infrastructure.dart';
 
 class AuthDatasourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -8,9 +13,18 @@ class AuthDatasourceImpl extends AuthDataSource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post(
+        '/auth/login',
+        data: {'email': email, 'password': password},
+      );
+
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
