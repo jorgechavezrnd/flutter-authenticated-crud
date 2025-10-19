c991284052347fdea7c0dbbd362d8a44af9257f0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-19 10:46:07 -0400

    Add implementation for handle errors

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -22,8 +22,15 @@ class AuthDatasourceImpl extends AuthDataSource {
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) throw WrongCredentials();
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw ConnectionTimeout();
+      }
+
+      throw CustomError('Something wrong happend', 1);
     } catch (e) {
-      throw WrongCredentials();
+      throw CustomError('Something wrong happend', 1);
     }
   }
 
--- lib/features/auth/infrastructure/errors/auth_errors.dart
+++ lib/features/auth/infrastructure/errors/auth_errors.dart
@@ -1,3 +1,12 @@
+class ConnectionTimeout implements Exception {}
+
 class WrongCredentials implements Exception {}
 
 class InvalidToken implements Exception {}
+
+class CustomError implements Exception {
+  final String message;
+  final int errorCode;
+
+  CustomError(this.message, this.errorCode);
+}
