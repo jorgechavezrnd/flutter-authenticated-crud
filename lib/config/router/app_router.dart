c8e64de2bb2c93fb50a48ec14bdde257c597ecad
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-23 22:40:19 -0400

    Add Check Auth Status Screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,8 +3,14 @@ import 'package:teslo_app/features/auth/auth.dart';
 import 'package:teslo_app/features/products/products.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/splash',
   routes: [
+    ///* Primera pantalla
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => const CheckAuthStatusScreen(),
+    ),
+
     ///* Auth Routes
     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(
