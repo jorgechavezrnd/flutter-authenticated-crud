6b480b5955ab4663fcd33c16ceb6ae3e95d4f390
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-26 00:46:13 -0400

    Add dependency for Flutter Staggered Grid View and base structure of Products Screen

--- lib/features/products/infrastructure/mappers/product_mapper.dart
+++ lib/features/products/infrastructure/mappers/product_mapper.dart
@@ -4,8 +4,8 @@ import 'package:teslo_app/features/products/domain/domain.dart';
 
 class ProductMapper {
   static Product jsonToEntity(Map<String, dynamic> json) => Product(
-    id: json['id,'],
-    title: json['title,'],
+    id: json['id'],
+    title: json['title'],
     price: double.parse(json['price'].toString()),
     description: json['description'],
     slug: json['slug'],
@@ -15,7 +15,7 @@ class ProductMapper {
     tags: List<String>.from(json['tags'].map((tag) => tag)),
     images: List<String>.from(
       json['images'].map(
-        (String image) => image.startsWith('http')
+        (image) => image.startsWith('http')
             ? image
             : '${Environment.apiUrl}/files/product/$image',
       ),
