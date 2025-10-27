6356abfac0ab805fb1a9d4637fd0094421699916
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-26 21:37:20 -0400

    Show snackbar after update product

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -10,6 +10,13 @@ class ProductScreen extends ConsumerWidget {
 
   const ProductScreen({super.key, required this.productId});
 
+  void showSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Producto Actualizado')));
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final productState = ref.watch(productProvider(productId));
@@ -33,7 +40,11 @@ class ProductScreen extends ConsumerWidget {
 
           ref
               .read(productFormProvider(productState.product!).notifier)
-              .onFormSubmit();
+              .onFormSubmit()
+              .then((value) {
+                if (!value) return;
+                showSnackbar(context);
+              });
         },
         child: const Icon(Icons.save_as_outlined),
       ),
